ea6e02cfd66ef313a979a9bfafee60c774337265
Juan Ignacio Donoso <jidonoso@gmail.com>
2012-01-23 18:43:54 -0300

    mas contenido

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+	// Add scroll spy to the top bar
+	$().scrollSpy();
+
+	// Get content from content directory
+	var apisTmpl = Hogan.compile($("#tmpl-apis").html());
+	var libsTmpl = Hogan.compile($("#tmpl-libs").html());
+	var othersTmpl = Hogan.compile($("#tmpl-others").html());
+	$.get("/content/apis.json", null, function(r){
+		_.each(r.apis, function(item){
+			$("#used-apis").append(apisTmpl.render(item));
+		});
+
+		_.each(r.libs, function(item){
+			$("#used-libs").append(libsTmpl.render(item));
+		});
+
+		_.each(r.others, function(item){
+			$("#used-others").append(othersTmpl.render(item));
+		});
+	})
+
+});
+
+
 GH = {
 	api: undefined,
 
@@ -8,9 +33,8 @@ GH = {
 	getRepos: function(){
 		this.api.getOrgRepos('huevapi', function(r){
 			var repoTmpl = Hogan.compile($("#tmpl-repo").html());
-			console.log(r);
 			_.each(r, function(repo){
-				$("#repositories").append(repoTmpl.render(repo));
+				$("#repo-list").append(repoTmpl.render(repo));
 			})
 		})
 	}
